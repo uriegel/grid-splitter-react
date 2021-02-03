1616f137e55581bf18a9af6b85dd86f55756f236
Uwe Riegel <uriegel@hotmail.de>
2021-02-03 07:16:19 +0100

    mix props in first view

--- example/src/HorizontalTest.jsx
+++ example/src/HorizontalTest.jsx
@@ -4,7 +4,10 @@ import { SplitterGrid } from 'grid-splitter-react'
 import 'grid-splitter-react/dist/index.css'
 
 const First = props => {
-	const call = () => props.setSecondInvisible(!props.props.isVisible)
+	const call = () => {
+		props.test()
+		props.setSecondInvisible(!props.props.isVisible)
+	}
 	return (
 	  	<div className="left" >
 			<div><button onClick = {call}>Klick</button></div>
@@ -15,15 +18,16 @@ const First = props => {
 const Second = () => <div className="right" />
 
 const App = () => {
-  return (
-    <div className="root">
-        <SplitterGrid first={(props, setSecondInvisible) => <First props={props} setSecondInvisible={setSecondInvisible} />} second={() => <Second />}/>
-    </div>
-  )}
+	const test = () => console.log("Test")
+  	return (
+    	<div className="root">
+        	<SplitterGrid first={(props, setSecondInvisible) => <First props={props} setSecondInvisible={setSecondInvisible} test={test}/>} second={() => <Second />}/>
+    	</div>
+)}
 
 // TODO: SetSecondVisible: from firstSplitter to App, button on top of Splittergrid:
 			// Call child from Parent
 // TODO: Initial second invisible
-// TODO: Button in first view to control second visiblility
+// TODO: Button in first view to control second visiblility: <First setRightVisible={setRightVisible} props={props} setSecondInvisible={setSecondInvisible} />
 
 export default App
